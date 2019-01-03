4935cb784b63623771c90620784f971164675eac
Damir Ismagilov <damiron123@gmail.com>
2019-01-03 19:19:05 +0300

    analyze methods

--- src/main/java/ClassAnalyzer.java
+++ src/main/java/ClassAnalyzer.java
@@ -1,5 +1,7 @@
 import com.intellij.psi.PsiClass;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -16,6 +18,35 @@ public class ClassAnalyzer {
         }
     }
 
+    private void analyzeMethods() {
+        if (aClass.getMethods() != null) {
+            Method[] methods = aClass.getMethods();
+            classInfo.put("methods", Arrays.stream(methods)
+                    .map(Method::getName)
+                    .collect(Collectors.toList()));
+            for (Method method : methods) {
+                classInfo.put(method.getName() + "Modifiers", Arrays.asList(Modifier.toString(method.getModifiers()).split(" ")));
+                classInfo.put(method.getName() + "ReturnType", Collections.singletonList(method.getReturnType().getCanonicalName()));
+                if (method.getParameterTypes() != null) {
+                    classInfo.put(method.getName() + "Parameters",
+                            Arrays.stream(method.getParameters())
+                                    .map(parameter -> parameter.getType().getCanonicalName() + " " + parameter.getName())
+                                    .collect(Collectors.toList()));
+                }
+                if (method.getExceptionTypes() != null) {
+                    classInfo.put(method.getName() + "Exceptions", Arrays.stream(method.getExceptionTypes())
+                            .map(Class::getCanonicalName)
+                            .collect(Collectors.toList()));
+                }
+                if (method.getDeclaredAnnotations() != null) {
+                    classInfo.put(method.getName() + "Annotations", Arrays.stream(method.getDeclaredAnnotations())
+                            .map(Annotation::toString)
+                            .collect(Collectors.toList()));
+                }
+            }
+        }
+    }
+
     private void analyze() {
         classInfo.put("name", Collections.singletonList(aClass.getCanonicalName()));
         classInfo.put("modifiers", Arrays.asList(Modifier.toString(aClass.getModifiers()).split(" ")));
@@ -25,8 +56,11 @@ public class ClassAnalyzer {
             classInfo.put("extends", Collections.singletonList("java.lang.Object"));
         }
         if (aClass.getInterfaces() != null) {
-            classInfo.put("implements", Arrays.stream(aClass.getInterfaces()).map(Class::getName).collect(Collectors.toList()));
+            classInfo.put("implements", Arrays.stream(aClass.getInterfaces())
+                                                    .map(Class::getName)
+                                                    .collect(Collectors.toList()));
         }
+        analyzeMethods();
     }
 
     public Map<String, List<String>> getClassInfo() {
