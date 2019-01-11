073b0d314a115ee68f82bb9ed73c27f4bd542bd6
Damir Ismagilov <damiron123@gmail.com>
2019-01-11 23:20:04 +0300

    delete unused imports

--- src/main/java/PdfComposer.java
+++ src/main/java/PdfComposer.java
@@ -1,15 +1,10 @@
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
-import gherkin.lexer.Pa;
 
-import javax.swing.*;
-import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.util.*;
 import java.util.List;
 import java.util.stream.Collectors;
